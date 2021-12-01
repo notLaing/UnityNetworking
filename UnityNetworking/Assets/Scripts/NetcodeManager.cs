8ebc4d6199d01f22ea74ea6f067dac3b6159df98
Justin Lai <laij3@ferris.edu>
2021-12-01 01:33:05 -0500

    Netcode package installed

---
+++ UnityNetworking/Assets/Scripts/NetcodeManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetcodeManager : MonoBehaviour
+{
+    public void OnGUI()
+    {
+        //runs at the start of the game and whenever UI has to do something
+        GUILayout.BeginArea(new Rect(10, 10, 300, 500));
+        if(!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            StartButtons();
+        }
+        else
+        {
+            StatusLabels();
+            SubmitNewPosition();
+        }
+
+        GUILayout.EndArea();
+    }
+
+    static void StartButtons()
+    {
+        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+        if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+    }
+
+    static void StatusLabels()
+    {
+        var mode = NetworkManager.Singleton.IsHost ? "Host" :
+            NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
+        GUILayout.Label("Mode: " + mode);
+    }
+
+    static void SubmitNewPosition()
+    {
+        string buttonLabel = NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change";
+        if(GUILayout.Button(buttonLabel))
+        {
+            //
+        }
+    }
+}
